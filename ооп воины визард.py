47625c6b918f5b6a6be6071a6eaba0b639f08109
shepnst <94540033+shepnst@users.noreply.github.com>
2022-02-08 18:25:09 +0300

    Add files via upload

--- ооп воины визард.py
+++ ооп воины визард.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from random import*
+from tkinter import*
+'''
 class Hero:
     def __init__(self, name, hp=100):
         self.health = hp
@@ -12,9 +15,8 @@ class Warrior(Hero):
        
     def fight(self, enemy):
         attack = self.attack*(1+randint(0, self.agility))
-            enemy.health -= attack
-            if isinstance()
-            print(self.name, 'ударил', s.name, 'на', self.attack)
+        enemy.health -= attack
+        print(self.name, 'ударил', enemy.name, 'на', self.attack)
         
 class Knight(Warrior):
     def __init__(self, name, hp=100, ap = 10, ag = 50):
@@ -23,42 +25,49 @@ class Knight(Warrior):
         
 class Ranger(Warrior):
     def __init__(self, name, hp=100, ap = 10, ag = 50):
-       super().__init__(self, name, hp, ap, ag)
-        self.agility = hp-20
-        self.attack = ap+5
+       super().__init__(name, hp, ap, ag)
+       self.agility = hp-20
+       self.attack = ap+5
         
-def fighting():
-    while len(s)>1:
-        a = choice(s)
-        b = choice(s)
+def fighting(ch):
+    while len(ch)>1:
+        a = choice(ch)
+        b = choice(ch)
         if a==b:
             print(a.name, 'наступил себе на ногу')
             continue
         a.fight(b)
         if b.health<=0:
-            s.remove(b)
+            ch.remove(b)
             print(b.name, 'убит!')
-    print(s[0].__dict__)
+    print(ch[0].__dict__)
     
     
-players = [Knight('Маша')]
+players = [Knight('Маша'), Ranger ('Дима'), ]
+for i in players:
+    print(i.__dict__)
 '''
-      
-class Wizard(Hero):
-    def __init__(self, name, mn = 12, hp = 100):
-        super().__init__(name, mn, hp)
-        self.manna = mn
-    
-        
-        
-      
-class Heeler(Wizard):
-    def __init__(self, ):
-        
-class Gandalf(Wizard):
-    def __init__(self, ):
-'''
-Vasya = Knight(name='Вася')
-Petya = Warrior(name = 'Петя')
-Vasya.fight(Petya)
-Petya.fight(Vasya)
+
+root = Tk()
+canva = Canvas(root, width =650, height =600 , bg='#73C87F')
+canva.pack()
+
+btn = Button(root,text = 'let`s start!', background = '#555', 
+             foreground = '#ccc', width = 8, height = 3)
+btn.pack()
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+#fighting(players)
+
+
+             
